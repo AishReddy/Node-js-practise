cee0530f77bcd40825e83cf0790d2a590b007094
P Aishwarya <paishwa@Aishwarya.local>
2019-06-26 22:02:42 +0530

    taking cmd args

---
+++ 04-file-system-cmd-line-args/4-1_getInputFromUser.js
@@ -0,0 +1,10 @@
+console.log(process.argv)
+
+//path to nodejs on your machine -different on individual machine 
+//path to your file where your file lives on your machine
+//the string which we provided in our program
+
+/*[ '/Users/paishwa/.nvm/versions/node/v8.9.1/bin/node',
+  '/Users/paishwa/Desktop/nodejs-practise/04-file-system-cmd-line-args/4-1_getInputFromUser.js',
+  'aish' ]*/
+  console.log(process.argv[2])
